6c0ef292ffd4d1e73935be4fd1967e3e9591cb3c
brighton-omondi <brightonomondi@yahoo.com>
2023-12-13 17:47:21 +0300

    adjusted the floating card to top

--- info.js
+++ info.js
@@ -4,11 +4,20 @@
 function closeCard() {
     var floatingCard = document.getElementById('floatingCard');
     floatingCard.style.display = 'none';
+    // Allow user interaction with the rest of the site after closing the card
+    document.body.style.pointerEvents = 'auto';
   }
   
-  // Show the floating card when the page loads
+  // Function to prevent user interaction with the site
+  function preventInteraction() {
+    // Prevent user interaction with the rest of the site until the card is closed
+    document.body.style.pointerEvents = 'none';
+  }
+  
+  // Show the floating card when the page loads and prevent interaction with the site
   window.onload = function() {
     var floatingCard = document.getElementById('floatingCard');
     floatingCard.style.display = 'block';
+    preventInteraction();
   };
   
