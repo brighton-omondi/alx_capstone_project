3d7caac8d9d4ea0de2ca7ede4b3fbfbcb3d9dde8
brighton-omondi <brightonomondi@yahoo.com>
2023-12-13 18:18:32 +0300

    restriction correction

--- info.js
+++ info.js
@@ -4,20 +4,11 @@
 function closeCard() {
     var floatingCard = document.getElementById('floatingCard');
     floatingCard.style.display = 'none';
-    // Allow user interaction with the rest of the site after closing the card
-    document.body.style.pointerEvents = 'auto';
   }
   
-  // Function to prevent user interaction with the site
-  function preventInteraction() {
-    // Prevent user interaction with the rest of the site until the card is closed
-    document.body.style.pointerEvents = 'none';
-  }
-  
-  // Show the floating card when the page loads and prevent interaction with the site
+  // Show the floating card when the page loads
   window.onload = function() {
     var floatingCard = document.getElementById('floatingCard');
     floatingCard.style.display = 'block';
-    preventInteraction();
   };
   
